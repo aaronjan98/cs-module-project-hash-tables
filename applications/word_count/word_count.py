8b63e46968336bcca4951df1df2e1df8d559a4f0
aaronjan <aaronjan98@gmail.com>
2020-06-04 14:30:24 -0700

    Update word_count.py

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,32 @@
-def word_count(s):
-    # Your code here
+ignored_char = ["\"", ":", ";", ",", ".", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&"]
+word_counter = {}
 
+def word_count(s):
+    # turn string to all lower case
+    lower_s = s.lower()
+    # splits the input string into an array of words
+    arr_of_words = lower_s.split()
+
+    # loop through the array of words and formats everything correctly
+    # and have a new arr of the correctly formatted words
+
+    for word in arr_of_words:
+        # loop through each letter of each word
+        for letter in range(len(word)):
+            # print(i[letter])
+            # if a letter in the word is a special char, delete it
+            if word[letter] in ignored_char:
+                # delete special character
+                # print('delete:', word[letter])
+                new_word = word.replace(word[letter], '')
+                print('new_word:', new_word)
+
+        # after looping through each letter in a word check if it exists in word_counter
+        value = arr_of_words.count(i)
+        word_counter.update({i: value})
+
+    print('after:', arr_of_words)
+    return word_counter
 
 
 if __name__ == "__main__":
